4d72ab8fdeb3bbd3072de2f3e2d2d1110d589f32
Shirialmog <shirlool@gmail.com>
2019-12-02 11:43:49 +0200

    Create folders

--- scan_to_slices.py
+++ scan_to_slices.py
@@ -1,24 +1,38 @@
+import os
+import numpy as np
+import nibabel as nb
+import matplotlib.pyplot as plt
+
+path= 'D:/Documents/ASchool/year 4/Deep learning project/ayelet_shiri/Spleen data'
+
 def scan_to_slices(path):
-    import os
-    import numpy as np
-    import nibabel as nb
-    import matplotlib.pyplot as plt
+    save_path='D:/Documents/ASchool/year 4/Deep learning project/ayelet_shiri/Prepared_Data'
+    os.mkdir(save_path+'/Spleen', 777)
+    files=os.listdir(path)
+    for file in files:
+        new_path = save_path + '/Spleen/' + file
+        os.mkdir(new_path, 777)
+        img = nb.load(path+'/'+file)
+        data = img.get_data()
+        num_slices = data.shape[2]
 
-    # import a single image
-    img = nb.load(path)
-    data = img.get_data()
-    num_slices = data.shape[2]
-
-    data = np.dstack((data[:, :, 0], data, data[:, :, num_slices - 1])) #padding the slices
-    output = np.empty((data.shape[0],data.shape[1],3), dtype=float, order='C')
+        data = np.dstack((data[:, :, 0], data, data[:, :, num_slices - 1])) #padding the slices
+        output = np.empty((data.shape[0],data.shape[1],3), dtype=float, order='C')
 
     # create a list of our "2.5D slices", each containing 3 slices
 
-    for i in range(1, num_slices+1):
-        output_new=output
-        output_new[:,:,1]=data[:,:,i] #main middle slice
-        output_new[:,:,0]=data[:,:,i-1] #bottom slice
-        output_new[:, :, 2] = data[:, :, i+1] #top slice
-        np.save('slice'+str(i), output_new)
+        for i in range(1, num_slices+1):
+            output_new=output
+            output_new[:,:,1]=data[:,:,i] #main middle slice
+            output_new[:,:,0]=data[:,:,i-1] #bottom slice
+            output_new[:, :, 2] = data[:, :, i+1] #top slice
+
+            np.save(new_path+'/slice'+str(i), output_new)
 
     return None
+
+def main(path):
+    scan_to_slices(path)
+
+if __name__ == '__main__':
+    main(path)
